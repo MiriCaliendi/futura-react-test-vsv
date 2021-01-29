723e53e6b3861cab9898112a31eb094a003b99ea
MiriCaliendi <67052935+MiriCaliendi@users.noreply.github.com>
2021-01-29 18:29:23 +0100

    complete point seven

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 const ALLCATEGORIESURL = 'https://api.chucknorris.io/jokes/categories'
-const RANDOMJOKEBYCATURL = 'https://api.chucknorris.io/jokes/random?category=' // remember to fill this
-const ALLLJOKESBYKEYWORD = 'https://api.chucknorris.io/jokes/search?query=' // remember to fill this
-const launchErrorAlert = () => setTimeout(() => window.alert('errore!'), 500) 
+//const RANDOMJOKEBYCATURL = 'https://api.chucknorris.io/jokes/random?category='
+const ALLLJOKESBYKEYWORD = 'https://api.chucknorris.io/jokes/search?query='
 
-// classe 'App-logo-spinning' durante il caricamento, altrimenti classe 'App-logo'
 const Logo = ({ loading }) => {
   return (
     <img
       src={logo}
+      className={`App-logo${loading ? ' App-logo-spinning' : " "}`}
       alt='interactive-logo'
-      className={`App-logo${this.state.loading ? " App-logo-spinning" : ""}`} 
     />
   )
 }
@@ -25,117 +23,160 @@ const CategoryButton = ({ title, onClick }) => {
   // </button>
 }
 
-
 const CategoriesList = ({ categories, onCategoryClick }) => {
   return null
   // per ciascun elemento di 'categories' renderizzare il componente <CategoryButton />
 }
 
 const Joke = ({ value, categories }) => {
-  // <div className="Joke">
-  //   <code className="Joke-Value">{value}</code>
-  //     per ciascun elemento di 'categories', renderizzare:
-  //     <span className="Selected-Cat" ... >
-  //       <code>{* QUI LA STRINGA DELLA SINGOLA CATEGORIA *}</code>
-  //     </span>
-  // </div>
+  return (
+    <div className="Joke">
+      <code className="Joke-Value">{categories}</code><br />
+      <span className={`Dont-View-Cat${categories === "" ? "Selected-Cat" : ""}`} >
+        <code >{value}</code>
+      </span>
+    </div>
+  )
 }
 
-class App extends React.Component {
-//function App() {
-  
-  // qui tutto ciò che serve al componente per essere inizializzato
-  constructor() {
-    super()
-    this.state = {
-      loading: false,
-      error: false,
-      categories: [],
-      selectedCategory: '',
-      fetchedJoke: {},
-      text: ""
+function App() {
+  const [text, setText] = useState('')
+  const [fetchedJoke, setFetchedJoke] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  const launchErrorAlert = (errore) => {
+    if (errore === true) {
+      (text === "") || alert("Error, incorrect value")
     }
   }
-  // getAllCategories
-  // funzione che deve recuperare l'array di tutte le categorie esistenti e salvarlo
-
+  // getRandomJokeByCat
+  // funzione che recupera una singola barzelletta e la salva
+  
   // onCategoryClick
   // funzione richiamata al click del componente CategoryButton
 
-  // getRandomJokeByCat
-  // funzione che recupera una singola barzelletta e la salva
+  const getAllCategories = async () => {
+    let joke = {}
+    let errore = false
+    let categoria = []
 
-  // getJokeByKeyword
-  // funzione che recupera le barzellette contenenti la parola chiave
-  // digitata nel campo di testo
-  getJokeByKeyword = () => {
-    const response = await 
-    fetch(`${ALLLJOKESBYKEYWORD}${this.state.text}`)
+    try {
+      setLoading(true)
+      const response = await fetch(ALLCATEGORIESURL)
+      let data = await response.json()
+      if (data.error) throw new Error(data.error)
+      joke = { ...data }
+
+    } catch (err) {
+      errore = true
+      console.log("Error: ", err)
+
+    } finally {
+      setLoading(false)
+      setError(errore)
+      //per punto 7
+      categoria = joke
+    }
   }
 
-  // onInputTextChange
-  // handler per l'input di testo
-  onInputTextChange = (event) => {
-    this.setState({text:event.target.value})
+  const getJokeByKeyword = async () => {
+    let joke = {}
+    let errore = false
+    let url = ""
+
+    try {
+      setLoading(true)
+      url = `${ALLLJOKESBYKEYWORD}${text}`
+      const response = await fetch(url)
+      let data = await response.json()
+      if (data.error) throw new Error(data.error)
+      joke = { ...data.result }
+      console.log(joke[0].value)
+
+    } catch (err) {
+      errore = true
+      console.log("Error: ", err)
+
+    } finally {
+      setLoading(false)
+      setError(errore)
+      if (errore === true) {
+        launchErrorAlert(errore)
+      } else {
+        setFetchedJoke(joke[0].value)
+      }
+    }
   }
 
-  // qui i lifecycle methods
+  const onInputTextChange = (event) => {
+    setText(event.target.value)
+    setFetchedJoke("")
+  }
 
-  render () {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <Logo
+  useEffect(() => {
+    getAllCategories()
+  }, [])
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Logo
+          loading={loading}
+        />
+        <input
+          type="search"
+          id="search"
+          name="search"
+          placeholder="Enter keyword here"
+          value={text}
+          onChange={onInputTextChange}
+        />
+        <button
+          className="Search-Button"
+          onClick={getJokeByKeyword}
+          type="button"
+        > <code>CLICK TO SEARCH!</code>
+        </button>
+        <code>or: </code>
+        <CategoriesList
             // ...
           />
-          <input
-            type="search"
-            id="search" name="search"
-            placeholder="Enter keyword here"
-            value= {this.state.text}
-            onChange = {this.onInputTextChange}
-          />
-          <button
-            className="Search-Button"
-            type="button"
-            onClick= {this.getJokeByKeyword}
-            
-          >
-            <code>CLICK TO SEARCH!</code>
-          </button>
-          <code>or: </code>
-          <CategoriesList
-            // ...
-          />
-        </div>
-        <div className="Content">
-          <img
-            src="https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png" 
-            className="Chuck-Logo"
-            alt="chuck-logo"
-          />
-          <code>
+      </div>
+      <div className="Content">
+        <img
+          src="https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png"
+          className="Chuck-Logo"
+          alt="chuck-logo"
+        />
+        <code>
+          {!error && (
             <h2>
               SELECTED CATEGORY:
               <span className="Selected-Cat">
-                {/* QUI LA CATEGORIA SELEZIONATA */}
+                {text}
               </span>
             </h2>
-          </code>
-          <button
-            className="Random-Button"
-            // ...
-          >
-            <h2>GET RANDOM JOKE FOR SELECTED CATEGORY</h2>
-          </button>
-          {/* <Joke ... /> */}
-        </div>
-        <div className="footer">
-        <code>Esame di React per cfp-futura. Grazie ad <a href="https://api.chucknorris.io">api.chucknorris.io</a> per l'immagine e le api. Docente: Vito Vitale. Studente: Miriana Caliendi  </code>
-        </div>
+          )}
+        </code> 
+        <button
+          className="Random-Button"
+          // ...
+        >
+          <h2>GET RANDOM JOKE FOR SELECTED CATEGORY</h2>
+        </button>
+        {text !== "" && (
+          <Joke
+            value={fetchedJoke}
+            categories={text}
+          />
+        )}
       </div>
-    );
-  }
+      <div className="footer">
+        <code>Esame di React per cfp-futura. Grazie ad <a href="https://api.chucknorris.io">api.chucknorris.io</a> per l'immagine e le api. Docente: Vito Vitale. Studente: Miriana Caliendi</code>
+      </div>
+    </div>
+  );
 };
 
-export default App;
+export default App;   
